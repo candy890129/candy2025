8a4614c5cb35b9d4e05c3e421af741f9b83212e0
candy <lyee890129@gmail.com>
2025-07-28 19:42:34 +0800

    單向綁定與雙向綁定

--- dist/js/20250728-2-vue-basic.js
+++ dist/js/20250728-2-vue-basic.js
@@ -5,6 +5,12 @@ let options = {
         return {
             account: "",
             password: "",
+            remember: false,
+            article: `This is a article <br>
+            <a href="https://www.google.com">Google</a>
+            <p>
+                <a href="https://www.google.com">Google</a>
+            </p>`,
         };
     },
     methods: {},
@@ -23,3 +29,8 @@ createApp(options).mount("#app");
 //     let showAccount = document.querySelector("#show-account");
 //     showAccount.innerHTML = account.value;
 // });
+
+// innerHTML -> v-html -> 會處理 html tag -> 效能差
+// textContent -> {{ article }} -> 不會處理 html tag -> 效能好
+// {{ article }} -> 單向綁定 -> html 無法更新資料
+// v-model -> 雙向綁定 -> html 可以更新資料
