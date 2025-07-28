5e1e203c9ea41ce1b1939a7383a91d9f8051bbe3
candy <lyee890129@gmail.com>
2025-07-28 18:53:34 +0800

    事件冒泡綁定

--- dist/js/20250723-1-filter-student-score.js
+++ dist/js/20250723-1-filter-student-score.js
@@ -43,10 +43,54 @@ let students = [
 
 let dom = {
     allTable: document.querySelector("#all-table"),
+    fn: document.querySelector("#fn"),
+    fnBtns: document.querySelectorAll("#fn .btn"),
 };
 
 console.log(students);
 console.log(dom);
+
+// 監聽 body 的 click 事件，確定事件冒泡有執行
+document.body.addEventListener("click", (e) => {
+    console.log("body click event");
+    console.log(e.target);
+});
+
+dom.fn.addEventListener("click", (e) => {
+    // 阻止事件冒泡
+    e.stopPropagation();
+    let target = e.target;
+    if (target.classList.contains("btn")) {
+        // 移除所有按鈕的 active 類別
+        dom.fnBtns.forEach((btn) => {
+            btn.classList.remove("active");
+        });
+
+        if (target.classList.contains("all")) {
+            console.log("is all btn");
+            target.classList.add("active");
+            // someting
+        }
+
+        if (target.classList.contains("pass")) {
+            console.log("is pass btn");
+            target.classList.add("active");
+            // someting
+        }
+
+        if (target.classList.contains("fail")) {
+            console.log("is fail btn");
+            target.classList.add("active");
+            // someting
+        }
+
+        if (target.classList.contains("both")) {
+            console.log("is both btn");
+            target.classList.add("active");
+            // someting
+        }
+    }
+});
 
 /**
  * 渲染所有學生資料
@@ -104,14 +148,19 @@ const renderPassTable = () => {
  * 渲染不及格學生資料
  * @returns {void}
  */
-const renderFailTable = () => {};
+const renderFailTable = () => {
+    let tbody = "";
+    let passes = filterStudent(false);
+    passes.forEach((student) => {
+        tbody += `<tr>
+    <td>${student.name}</td>
+        <td>${student.score}</td>
+        </tr>`;
+    });
 
-console.log("-----fails-----");
-console.table(fails);
-console.log("-----passes-----");
-console.table(passes);
-console.log("-----students-----");
-console.table(students);
+    dom.allTable.querySelector("tbody").innerHTML = tbody;
+};
 
 // renderAllTable();
 // renderPassTable();
+// renderFailTable();
