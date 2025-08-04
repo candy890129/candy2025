5b41bfcdee3a6c75b60729de74579234c36fd2ec
candy <lyee890129@gmail.com>
2025-08-04 19:22:36 +0800

    v

--- dist/js/todo-app.js
+++ dist/js/todo-app.js
@@ -2,11 +2,36 @@ const { createApp } = Vue;
 
 const options = {
     data() {
-        return {};
+        return {
+            processText: "",
+            processList: [],
+        };
     },
-    methods: {},
+    methods: {
+        addProcess() {
+            // 當沒有輸入時，不新增
+            if (!this.processText) {
+                return;
+            }
+
+            // 新增代辦事項
+            this.processList.push({
+                text: this.processText,
+            });
+
+            // 清空輸入框
+            this.processText = "";
+        },
+    },
     mounted() {
         console.log("mounted");
+        this.processList.push({
+            text: "1111aaaaa",
+        });
+        this.processList.push({
+            text: "2222bbbbb",
+        });
+        console.log(this.processList);
     },
 };
 
