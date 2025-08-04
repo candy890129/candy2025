d20e3e6399c1873e8e4cb2f52535b5683c52baba
candy <lyee890129@gmail.com>
2025-08-04 19:47:40 +0800

    進行中列表儲存

--- dist/js/todo-app.js
+++ dist/js/todo-app.js
@@ -81,3 +81,65 @@ createApp(options).mount("#app");
 const studentJson = localStorage.getItem("student");
 const student = JSON.parse(studentJson);
 console.log(student, student.name);
+
+import { Storage } from "./components/Storage.js";
+
+const todoStorage = new Storage("todo");
+
+const { createApp } = Vue;
+
+const options = {
+    data() {
+        return {
+            processText: "",
+            processList: [],
+            processDoneList: [],
+        };
+    },
+    methods: {
+        addProcess() {
+            // 當沒有輸入時，不新增
+            if (!this.processText) {
+                return;
+            }
+
+            // 新增代辦事項
+            this.processList.push({
+                text: this.processText,
+            });
+
+            // 清空輸入框
+            this.processText = "";
+
+            // 儲存資料
+            todoStorage.write(this.processList);
+        },
+        toDone(index) {
+            // 將資料拿出
+            let item = this.processList[index];
+
+            // 推到完成列表
+            this.processDoneList.push(item);
+
+            // 刪除原本的資料
+            this.processList.splice(index, 1);
+        },
+        toProcess(index) {
+            // 將資料拿出
+            let item = this.processDoneList[index];
+
+            // 推到進行中列表
+            this.processList.push(item);
+
+            // 刪除原本的資料
+            this.processDoneList.splice(index, 1);
+        },
+    },
+    mounted() {
+        console.log("mounted");
+        this.processList = todoStorage.read([]);
+        console.log(this.processList, typeof this.processList);
+    },
+};
+
+createApp(options).mount("#app");
