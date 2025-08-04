ba5e2b9988e0d8073445d442eabc0e37757f579f
candy <lyee890129@gmail.com>
2025-08-04 20:12:33 +0800

    重購項目結構

--- dist/js/todo-app.js
+++ dist/js/todo-app.js
@@ -1,3 +1,7 @@
+import { Storage } from "./components/Storage.js";
+
+const todoStorage = new Storage("todo");
+
 const { createApp } = Vue;
 
 const options = {
@@ -5,7 +9,6 @@ const options = {
         return {
             processText: "",
             processList: [],
-            processDoneList: [],
         };
     },
     methods: {
@@ -17,52 +20,66 @@ const options = {
 
             // 新增代辦事項
             this.processList.push({
+                id: Date.now(),
                 text: this.processText,
+                status: "process",
             });
 
             // 清空輸入框
             this.processText = "";
+
+            // 儲存資料
+            todoStorage.write(this.processList);
         },
-        toDone(index) {
-            // 將資料拿出
-            let item = this.processList[index];
+        toDone(id) {
+            // 將資料拿出，call by reference
+            let items = this.processList.filter((item) => {
+                return item.id === id;
+            });
 
-            // 推到完成列表
-            this.processDoneList.push(item);
+            if (items.length > 0) {
+                items[0].status = "done";
+                todoStorage.write(this.processList);
+            }
 
-            // 刪除原本的資料
-            this.processList.splice(index, 1);
+            // // 推到完成列表
+            // this.processDoneList.push(item);
+
+            // // 刪除原本的資料
+            // this.processList.splice(index, 1);
         },
-        toProcess(index) {
-            // 將資料拿出
-            let item = this.processDoneList[index];
+        toProcess(id) {
+            // 將資料拿出，call by reference
+            let items = this.processList.filter((item) => {
+                return item.id === id;
+            });
 
-            // 推到進行中列表
-            this.processList.push(item);
+            if (items.length > 0) {
+                items[0].status = "process";
+                todoStorage.write(this.processList);
+            }
 
-            // 刪除原本的資料
-            this.processDoneList.splice(index, 1);
+            // // 推到進行中列表
+            // this.processList.push(item);
+
+            // // 刪除原本的資料
+            // this.processDoneList.splice(index, 1);
+        },
+        processItems() {
+            return this.processList.filter((item) => {
+                return item.status === "process";
+            });
+        },
+        doneItems() {
+            return this.processList.filter((item) => {
+                return item.status === "done";
+            });
         },
     },
     mounted() {
         console.log("mounted");
-        this.processList.push({
-            text: "1111aaaaa",
-        });
-        this.processList.push({
-            text: "2222bbbbb",
-        });
-        console.log(this.processList);
-
-        // mock data
-        this.processDoneList.push({
-            text: "1111done",
-        });
-        this.processDoneList.push({
-            text: "2222done",
-        });
-
-        console.log(this.processDoneList);
+        this.processList = todoStorage.read([]);
+        console.log(this.processList, typeof this.processList);
     },
 };
 
@@ -81,65 +98,3 @@ createApp(options).mount("#app");
 const studentJson = localStorage.getItem("student");
 const student = JSON.parse(studentJson);
 console.log(student, student.name);
-
-import { Storage } from "./components/Storage.js";
-
-const todoStorage = new Storage("todo");
-
-const { createApp } = Vue;
-
-const options = {
-    data() {
-        return {
-            processText: "",
-            processList: [],
-            processDoneList: [],
-        };
-    },
-    methods: {
-        addProcess() {
-            // 當沒有輸入時，不新增
-            if (!this.processText) {
-                return;
-            }
-
-            // 新增代辦事項
-            this.processList.push({
-                text: this.processText,
-            });
-
-            // 清空輸入框
-            this.processText = "";
-
-            // 儲存資料
-            todoStorage.write(this.processList);
-        },
-        toDone(index) {
-            // 將資料拿出
-            let item = this.processList[index];
-
-            // 推到完成列表
-            this.processDoneList.push(item);
-
-            // 刪除原本的資料
-            this.processList.splice(index, 1);
-        },
-        toProcess(index) {
-            // 將資料拿出
-            let item = this.processDoneList[index];
-
-            // 推到進行中列表
-            this.processList.push(item);
-
-            // 刪除原本的資料
-            this.processDoneList.splice(index, 1);
-        },
-    },
-    mounted() {
-        console.log("mounted");
-        this.processList = todoStorage.read([]);
-        console.log(this.processList, typeof this.processList);
-    },
-};
-
-createApp(options).mount("#app");
