edd56eacb2d6765b2c01b6a6707f5959cbebcc5b
candy <lyee890129@gmail,com>
2025-08-13 19:27:25 +0800

    header active 動態附加

--- dist/js/partner.js
+++ dist/js/partner.js
@@ -2,4 +2,4 @@ import { Loader } from "./components/Loader.js";
 import { Header } from "./components/Header.js";
 
 await Loader.html("components/header.html", "#header-container");
-Header.activeMenu("index");
+Header.activeMenu("partner");
